9ff4dc31b26b129aecdac46d08d8ffa799fb44c0
Daniel Forde <daniel.forde@sioux.eu>
2026-09-21 19:34:40 +0200

    Average many samples over the 30 minute window to create a better representation of the power production

--- src/solaredge/main.py
+++ src/solaredge/main.py
@@ -1,4 +1,4 @@
-from time import monotonic, sleep
+from time import time, sleep
 from pprint import pprint
 
 from solaredge.database.database import Database
@@ -8,15 +8,38 @@ SAMPLE_PERIOD = 30 * 60  # seconds
 
 def main() -> None:
     with SunSpecReader() as reader, Database() as database:
-        last_sample_timestamp = 0
+        now = time()
+        next_sample_timestamp = (int(now) // SAMPLE_PERIOD + 1) * SAMPLE_PERIOD
+        averages = {}
+
         while True:
-            now = monotonic()
-            if now - last_sample_timestamp >= SAMPLE_PERIOD:
-                data = reader.read_inverter_measurements()
+            data = reader.read_inverter_measurements()
+            for k, v in data.items():
+                if k != "timestamp":
+                    if k in averages:
+                        cnt = averages[k]["cnt"]
+                        avg = averages[k]["avg"]
+                        cnt += 1
+                        avg = avg + (v - avg) / cnt
+                        averages[k]["cnt"] = cnt
+                        averages[k]["avg"] = avg
+                    else:
+                        averages[k] = {
+                            "avg": v,
+                            "cnt": 1
+                        }
+
+            now = time()
+            if now >= next_sample_timestamp:
+                data = {k: v["avg"] for k, v in averages.items()}
+                data["timestamp"] = next_sample_timestamp
                 database.add_measurement(data)
-                # pprint(data)
-                last_sample_timestamp = now
-            sleep(SAMPLE_PERIOD - (now - last_sample_timestamp))
+                averages = {}
+                pprint(data)
+                while next_sample_timestamp <= now:
+                    next_sample_timestamp += SAMPLE_PERIOD
+
+            sleep(10)
 
 if __name__ == "__main__":
     main()
